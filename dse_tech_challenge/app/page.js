27969f181c232efd45b0659e39dadac7ac046f72
juliomenard <juliomenard@gmail.com>
2024-04-22 11:24:50 -0400

    updated the reference to my client id, updated the name of the component from Home to App, modified the page with App to be client side rendered to use the finch front end SDK code, and added a an api route for finch and nested a folder named callback which is currently empty

--- dse_tech_challenge/app/page.js
+++ dse_tech_challenge/app/page.js
@@ -1,9 +1,26 @@
-export default function Home() {
-  // const handleClick = async () => {
-  //   try {
-  //     const response = await fetch()
-  //   }
-  // }
+'use client'
+
+import React, { useState } from "react";
+import { useFinchConnect } from "@tryfinch/react-connect";
+
+export default function App() {
+  const [code, setCode] = useState(null);
+
+  const onSuccess = ({ code }) => setCode(code);
+  const onError = ({ errorMessage }) => console.error(errorMessage);
+  const onClose = () => console.log("User exited Finch Connect");
+
+  const { open } = useFinchConnect({
+    clientId: process.env.CLIENT_ID,
+    products: ["company", "directory", "individual", "employment"],
+    payrollProvider: "gusto", // optional
+    sandbox: "provider",       // optional
+    state: "",                // optional
+    onSuccess,
+    onError,
+    onClose,
+  });
+
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
@@ -12,8 +29,8 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
        Create Gusto Sandbox now!
       </div>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg shadow-md transition duration-300 ease-in-out">
-  Create
+      <button onClick={() => open()} class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg shadow-md transition duration-300 ease-in-out">
+      Open Finch Connect
 </button>
 
     </main>
