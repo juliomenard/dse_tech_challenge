e5035da088d6fead038fbfbc09ccb851058452ad
juliomenard <juliomenard@gmail.com>
2024-04-22 01:39:33 -0400

    installed Finch's React SDK, and using this commit to test my .gitignore file

--- dse_tech_challenge/app/page.js
+++ dse_tech_challenge/app/page.js
@@ -1,11 +1,21 @@
-import Image from "next/image";
-
 export default function Home() {
+  // const handleClick = async () => {
+  //   try {
+  //     const response = await fetch()
+  //   }
+  // }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
-       Hello welcome to this app. I'll be your developer
+       Hello! Welcome to the Gusto sandbox hosted by Finch
       </div>
+      <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
+       Create Gusto Sandbox now!
+      </div>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg shadow-md transition duration-300 ease-in-out">
+  Create
+</button>
+
     </main>
   );
 }
